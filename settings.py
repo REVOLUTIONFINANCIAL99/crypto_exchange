b78fe7ccd346701e4508a1f3d50afed50685d47c
MARC <marcvgonsal@gmail.com>
2024-11-19 10:58:01 +0100

    Descripción de los cambios realizados

--- settings.py
+++ settings.py
@@ -1,4 +1,13 @@
 INSTALLED_APPS = [
-    ...
-    'exchange',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'users',  # Aplicación de usuarios
+    'transactions',  # Aplicación de transferencias
+    'contracts',# Aplicación de contratos
+    'exchange'# 
 ]
+AUTH_USER_MODEL = 'users.CustomUser'
